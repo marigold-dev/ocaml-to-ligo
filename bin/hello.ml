ce0e06fe71fa533d514b354a0df309f791a5088e
Andre Popovitch <andre@popovit.ch>
2021-12-01 18:53:59 +0000

    add stdlib

--- bin/hello.ml
+++ bin/hello.ml
@@ -109,7 +109,7 @@ let loc = Location.none
 
 let code =
   [%str
-    let a = 1
+    let a = Tezos.level
     let b = a
     let add a = a + 1
     let rec add' (a, b) = a + b
@@ -183,7 +183,7 @@ and stringify_structure : int -> Typedtree.structure_item list -> string =
   |> List.map (( ^ ) (repeat (indentation * 2) " "))
   |> String.concat "\n"
 
-let type_structure structure =
+let type_structure env structure =
   match
     !Typecore.type_module env
       {
@@ -197,11 +197,91 @@ let type_structure structure =
         pmod_attributes = [];
       }
   with
-  | { mod_desc = Tmod_structure { str_items = module_expr; _ }; _ } ->
-      module_expr
+  | { mod_desc = Tmod_structure { str_items = module_expr; str_final_env; _ }; _ } ->
+      (module_expr, str_final_env)
   | _ -> failwith "impossible"
 (* let tcode = Typecore.type_exp env code *)
 
 (* let scode = to_expression tcode |> Format.printf "%a\n%!" Pprintast.expression *)
 
-let () = stringify_structure 0 (code |> type_structure) |> print_endline
+
+let stdlib = [%str 
+
+type nat = nativeint
+
+type (_, _) big_map
+
+type (_, _) map
+
+type _ set
+
+type mutez = nat
+
+type tez = nat
+
+type operation
+
+type address
+
+type _ contract
+
+let min : nat -> nat -> nat = assert false
+let abs : int -> nat = assert false
+
+module Tezos = struct
+  let level : nat = assert false
+
+  let amount : tez = assert false
+
+  let sender : address = assert false
+
+  let transaction : 'parameter -> mutez -> 'parameter contract -> operation =
+    assert false
+
+  let get_contract_opt : address -> 'parameter contract option = assert false
+end
+
+module rec Big_map : sig
+  val empty : ('key, 'value) big_map
+
+  val find_opt : 'key -> ('key, 'value) big_map -> 'value option
+
+  val add : 'key -> 'value -> ('key, 'value) big_map -> ('key, 'value) big_map
+
+  val remove : 'key -> ('key, 'value) big_map -> ('key, 'value) big_map
+
+  val mem : 'key -> ('key, 'value) big_map -> bool
+end = struct
+  let empty = assert false
+
+  let find_opt = assert false
+
+  let add = assert false
+
+  let remove = assert false
+
+  let mem = assert false
+end
+
+module rec Set : sig
+  val add : 'el -> 'el set -> 'el set
+
+  val empty : 'a set
+
+  val cardinal : 'a set -> nat
+
+  val fold : ('acc * 'el -> 'acc) -> 'el set -> 'acc -> 'acc
+end = struct
+  let add = assert false
+
+  let empty = assert false
+
+  let cardinal = assert false
+
+  let fold = assert false
+end
+
+]
+let env = type_structure env stdlib |> snd
+
+let () = stringify_structure 0 (code |> type_structure env |> fst) |> print_endline
