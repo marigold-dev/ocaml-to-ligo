cc68b416ebdcfcfd359960e9b5c0a4069a42dc24
Ambika Eshwar <ambika@rosalogia.me>
2021-12-03 14:17:40 -0500

    Style changes, formatting, tconstraint is smarter now

--- lib/o2l.ml
+++ lib/o2l.ml
@@ -46,15 +46,29 @@ and ct_desc_of_te te : Parsetree.core_type_desc option =
       Some (Parsetree.Ptyp_constr (to_lident path, args))
   | Tlink typ | Tsubst typ -> ct_desc_of_te typ
   | Tvar _ -> None
-  | Tpoly (expr, _) -> let* expr = ct_of_te expr in Some (Parsetree.Ptyp_poly ([], expr))
-  | Tobject _ -> let () = Printf.printf "Tobject\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tfield _ -> let () = Printf.printf "Tfield\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tnil -> let () = Printf.printf "Tnil\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tvariant _ -> let () = Printf.printf "Tvariant\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tunivar _ -> let () = Printf.printf "Tunivar\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tpackage _ -> let () = Printf.printf "Tpackage\n" in unimplemented Printtyp.type_expr te __LINE__
-  
-  (*
+  | Tpoly (expr, _) ->
+      let* expr = ct_of_te expr in
+      Some (Parsetree.Ptyp_poly ([], expr))
+  | Tobject _ ->
+      Printf.printf "Tobject\n";
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tfield _ ->
+      Printf.printf "Tfield\n";
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tnil ->
+      Printf.printf "Tnil\n";
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tvariant _ ->
+      Printf.printf "Tvariant\n";
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tunivar _ ->
+      Printf.printf "Tunivar\n";
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tpackage _ ->
+      Printf.printf "Tpackage\n";
+      unimplemented Printtyp.type_expr te __LINE__
+
+(*
   | Tlink typ | Tsubst typ -> ct_desc_of_te typ
   | Tvar _ -> None
   | Tobject _ -> failwith "Tobject"
@@ -65,27 +79,33 @@ and ct_desc_of_te te : Parsetree.core_type_desc option =
   | Tpackage _ -> failwith "Tpackage"
   *)
 
-(* Defines a custom "mapper" that transforms any Typedtree type
-   to its corresponding Parsetree type, but with explicit types *)
-and mapper =
+(* Defines a custom Untypeast.mapper that transforms any Typedtree type
+   to its corresponding Parsetree type, but with explicit types if
+   it doesn't have type annotations already *)
+
+let rec mapper =
   (* Takes a Typedtree pattern and transforms it into
      a corresponding Parsetree pattern that's "constrained",
      i.e. includes an explicit type annotation *)
   let tconstrain_pattern (loc : Warnings.loc)
       (pattern : 'a Typedtree.general_pattern) =
     match ct_of_te pattern.pat_type with
-    | Some ct ->
-        Ast_helper.Pat.constraint_ (Untypeast.untype_pattern pattern) ct
+    | Some ct -> (
+        (* Check that the pattern does not already contain a
+           type annotation. If it does, do not add the type constraint *)
+        match pattern.pat_extra with
+        | (Tpat_constraint _, _, _) :: _ -> Untypeast.untype_pattern pattern
+        | _ -> Ast_helper.Pat.constraint_ (Untypeast.untype_pattern pattern) ct)
     | None ->
         failwith
           (Format.asprintf
-             "You have a type variable at %a, you must annotate it with \
-              a concrete type"
+             "You have a type variable at %a, you must annotate it with a \
+              concrete type"
              Location.print_loc loc)
   in
   let untype_expression_expanded mapper expr =
-    let loc = Typedtree.(expr.exp_loc) in
-    let tconstrain_pattern = tconstrain_pattern loc in
+    let open Typedtree in
+    let tconstrain_pattern = expr.exp_loc |> tconstrain_pattern in
     let recurse = Untypeast.(mapper.expr) mapper in
     let open Typedtree in
     match expr.exp_desc with
@@ -117,22 +137,17 @@ and mapper =
   in
   let untype_structure_item_expanded (mapper : Untypeast.mapper) structure :
       Parsetree.structure_item =
-    match structure with
-    | Typedtree.
+    let open Typedtree in
+    match structure.str_desc with
+    | Tstr_module
         {
-          str_desc =
-            Tstr_module
-              {
-                mb_name;
-                mb_expr =
-                  { mod_desc = Tmod_constraint ((_ as mod_expr), _, _, _); _ };
-                _;
-              };
+          mb_name;
+          mb_expr = { mod_desc = Tmod_constraint ((_ as mod_expr), _, _, _); _ };
           _;
-        } ->        mod_expr |> mapper.module_expr mapper |> Ast_helper.Mb.mk mb_name
+        } ->
+        mod_expr |> mapper.module_expr mapper |> Ast_helper.Mb.mk mb_name
         |> Ast_helper.Str.module_
-        (*
-    | Typedtree.{ str_desc = Tstr_value (recflag, value_bindings); _ } ->
+    | Tstr_value (recflag, value_bindings) ->
         let value_bindings =
           List.map
             (fun binding ->
@@ -143,8 +158,8 @@ and mapper =
                 })
             value_bindings
         in
-        value_bindings |> Ast_helper.Str.value recflag*)
-    | m -> Untypeast.default_mapper.structure_item mapper m
+        value_bindings |> Ast_helper.Str.value recflag
+    | _ -> Untypeast.default_mapper.structure_item mapper structure
   in
   let untype_structure_expanded mapper Typedtree.{ str_items; _ } =
     List.map (untype_structure_item_expanded mapper) str_items
@@ -158,12 +173,18 @@ and mapper =
 
 and untype_expression a = Untypeast.untype_expression ~mapper a
 
+(* User facing function that maps typed_string_of_struct to a list
+   of structure items and joins them with newlines. Essentially this
+   processes an arbitrary snippet of OCaml code *)
+let typed_string_of_code struc =
+  struc |> mapper.structure mapper |> Format.asprintf "%a" Pprintast.structure
 
-and typed_string_of_code : Typedtree.structure -> string =
- fun struc -> struc |> mapper.structure mapper |> Format.asprintf "%a" Pprintast.structure 
+let default_environment =
+  Compmisc.init_path ();
+  Compmisc.initial_env ()
 
-let type_structure ~env structure =
-  match
+let type_structure ?(env = default_environment) structure =
+  let typed_module =
     !Typecore.type_module env
       {
         pmod_desc = Pmod_structure structure;
@@ -175,25 +196,17 @@ let type_structure ~env structure =
           };
         pmod_attributes = [];
       }
-  with
-  | { mod_desc = Tmod_structure ({ str_final_env; _ } as structure); _ } ->
+  in
+  match typed_module.mod_desc with
+  | Tmod_structure ({ str_final_env; _ } as structure)
+  | Tmod_constraint
+      ( { mod_desc = Tmod_structure ({ str_final_env; _ } as structure); _ },
+        _,
+        _,
+        _ ) ->
       (structure, str_final_env)
-  | {
-   mod_desc =
-     Tmod_constraint
-       ( { mod_desc = Tmod_structure ({  str_final_env; _ } as structure); _ },
-         _,
-         _,
-         _ );
-   _;
-  } ->
-      (structure, str_final_env)
-  | { mod_desc; _ } -> (
-      match mod_desc with
-      | Tmod_ident _ -> failwith "Tmod_ident impossible"
-      | Tmod_structure _ -> failwith "Tmod_structure impossible"
-      | Tmod_functor _ -> failwith "Tmod_functor impossible"
-      | Tmod_apply _ -> failwith "Tmod_apply impossible"
-      | Tmod_constraint _ -> failwith "Tmod_constraint impossible"
-      | Tmod_unpack _ -> failwith "Tmod_unpack impossible")
-
+  | Tmod_ident _ -> failwith "Tmod_ident impossible"
+  | Tmod_functor _ -> failwith "Tmod_functor impossible"
+  | Tmod_apply _ -> failwith "Tmod_apply impossible"
+  | Tmod_constraint _ -> failwith "Tmod_constraint impossible"
+  | Tmod_unpack _ -> failwith "Tmod_unpack impossible"
