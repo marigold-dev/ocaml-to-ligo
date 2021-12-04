517d8b234329921c289c33d1d814572991a657bf
Andre Popovitch <andre@popovit.ch>
2021-12-04 02:39:09 +0000

    I think this works

--- lib/o2l.ml
+++ lib/o2l.ml
@@ -46,15 +46,29 @@ and ct_desc_of_te te : Parsetree.core_type_desc option =
       Some (Parsetree.Ptyp_constr (to_lident path, args))
   | Tlink typ | Tsubst typ -> ct_desc_of_te typ
   | Tvar _ -> None
-  | Tpoly (expr, _) -> let* expr = ct_of_te expr in Some (Parsetree.Ptyp_poly ([], expr))
-  | Tobject _ -> let () = Printf.printf "Tobject\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tfield _ -> let () = Printf.printf "Tfield\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tnil -> let () = Printf.printf "Tnil\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tvariant _ -> let () = Printf.printf "Tvariant\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tunivar _ -> let () = Printf.printf "Tunivar\n" in unimplemented Printtyp.type_expr te __LINE__
-  | Tpackage _ -> let () = Printf.printf "Tpackage\n" in unimplemented Printtyp.type_expr te __LINE__
-  
-  (*
+  | Tpoly (expr, _) ->
+      let* expr = ct_of_te expr in
+      Some (Parsetree.Ptyp_poly ([], expr))
+  | Tobject _ ->
+      let () = Printf.printf "Tobject\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tfield _ ->
+      let () = Printf.printf "Tfield\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tnil ->
+      let () = Printf.printf "Tnil\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tvariant _ ->
+      let () = Printf.printf "Tvariant\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tunivar _ ->
+      let () = Printf.printf "Tunivar\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+  | Tpackage _ ->
+      let () = Printf.printf "Tpackage\n" in
+      unimplemented Printtyp.type_expr te __LINE__
+
+(*
   | Tlink typ | Tsubst typ -> ct_desc_of_te typ
   | Tvar _ -> None
   | Tobject _ -> failwith "Tobject"
@@ -67,25 +81,29 @@ and ct_desc_of_te te : Parsetree.core_type_desc option =
 
 (* Defines a custom "mapper" that transforms any Typedtree type
    to its corresponding Parsetree type, but with explicit types *)
+
 and mapper =
   (* Takes a Typedtree pattern and transforms it into
      a corresponding Parsetree pattern that's "constrained",
      i.e. includes an explicit type annotation *)
-  let tconstrain_pattern (loc : Warnings.loc)
-      (pattern : 'a Typedtree.general_pattern) =
-    match ct_of_te pattern.pat_type with
-    | Some ct ->
-        Ast_helper.Pat.constraint_ (Untypeast.untype_pattern pattern) ct
-    | None ->
-        failwith
-          (Format.asprintf
-             "You have a type variable at %a, you must annotate it with \
-              a concrete type"
-             Location.print_loc loc)
+  let tconstrain_pattern
+        : 'a.
+          Untypeast.mapper -> 'a Typedtree.general_pattern -> Parsetree.pattern
+      =
+   fun _mapper pattern ->
+    match Untypeast.default_mapper.pat Untypeast.default_mapper pattern with
+    | {ppat_desc=Ppat_constraint _; _} as untyped_pat -> untyped_pat
+    | untyped_pat -> (
+        match ct_of_te pattern.pat_type with
+        | Some ct -> Ast_helper.Pat.constraint_ untyped_pat ct
+        | None ->
+            failwith
+              (Format.asprintf
+                 "You have a type variable at %a, you must annotate it with a \
+                  concrete type"
+                 Location.print_loc pattern.pat_loc))
   in
   let untype_expression_expanded mapper expr =
-    let loc = Typedtree.(expr.exp_loc) in
-    let tconstrain_pattern = tconstrain_pattern loc in
     let recurse = Untypeast.(mapper.expr) mapper in
     let open Typedtree in
     match expr.exp_desc with
@@ -93,7 +111,7 @@ and mapper =
         let untyped_value_binding =
           Parsetree.
             {
-              pvb_pat = tconstrain_pattern vb.vb_pat;
+              pvb_pat = mapper.pat mapper vb.vb_pat;
               pvb_expr = recurse vb.vb_expr;
               pvb_attributes = vb.vb_attributes;
               pvb_loc = vb.vb_loc;
@@ -104,9 +122,9 @@ and mapper =
         let pattern, guard, body =
           match cases with
           | [ { c_lhs; c_guard; c_rhs } ] ->
-              (tconstrain_pattern c_lhs, c_guard, c_rhs)
+              (tconstrain_pattern mapper c_lhs, c_guard, c_rhs)
           | _ ->
-              let pexpr = untype_expression expr in
+              let pexpr = mapper.expr mapper expr in
               Format.printf "function not supported. Representation is %a\n"
                 (Printast.expression 0) pexpr;
               unimplemented Pprintast.expression pexpr __LINE__
@@ -129,21 +147,47 @@ and mapper =
                 _;
               };
           _;
-        } ->        mod_expr |> mapper.module_expr mapper |> Ast_helper.Mb.mk mb_name
+        } ->
+        mod_expr |> mapper.module_expr mapper |> Ast_helper.Mb.mk mb_name
         |> Ast_helper.Str.module_
-        (*
     | Typedtree.{ str_desc = Tstr_value (recflag, value_bindings); _ } ->
         let value_bindings =
           List.map
             (fun binding ->
-              Parsetree.
-                {
-                  (mapper.value_binding mapper binding) with
-                  pvb_pat = tconstrain_pattern binding.vb_loc binding.vb_pat;
-                })
+              match mapper.value_binding mapper binding with
+              | { pvb_pat = { ppat_desc = Ppat_constraint _; _ }; _ } as
+                untyped_binding ->
+                  untyped_binding
+              | untyped_binding -> (
+                  match ct_of_te Typedtree.(binding.vb_expr.exp_type) with
+                  | Some ct ->
+                      Parsetree.
+                        {
+                          untyped_binding with
+                          pvb_pat =
+                            {
+                              untyped_binding.pvb_pat with
+                              ppat_desc =
+                                Ppat_constraint
+                                  ( untyped_binding.pvb_pat,
+                                    Ptyp_poly ([], ct) |> Ast_helper.Typ.mk );
+                            };
+                          pvb_expr =
+                            {
+                              untyped_binding.pvb_expr with
+                              pexp_desc =
+                                Pexp_constraint (untyped_binding.pvb_expr, ct);
+                            };
+                        }
+                  | None ->
+                      failwith
+                        (Format.asprintf
+                           "You have a type variable at %a, you must annotate \
+                            it with a concrete type"
+                           Location.print_loc binding.vb_loc)))
             value_bindings
         in
-        value_bindings |> Ast_helper.Str.value recflag*)
+        value_bindings |> Ast_helper.Str.value recflag
     | m -> Untypeast.default_mapper.structure_item mapper m
   in
   let untype_structure_expanded mapper Typedtree.{ str_items; _ } =
@@ -156,11 +200,9 @@ and mapper =
     structure = untype_structure_expanded;
   }
 
-and untype_expression a = Untypeast.untype_expression ~mapper a
-
-
 and typed_string_of_code : Typedtree.structure -> string =
- fun struc -> struc |> mapper.structure mapper |> Format.asprintf "%a" Pprintast.structure 
+ fun struc ->
+  struc |> mapper.structure mapper |> Format.asprintf "%a" Pprintast.structure
 
 let type_structure ~env structure =
   match
@@ -181,7 +223,7 @@ let type_structure ~env structure =
   | {
    mod_desc =
      Tmod_constraint
-       ( { mod_desc = Tmod_structure ({  str_final_env; _ } as structure); _ },
+       ( { mod_desc = Tmod_structure ({ str_final_env; _ } as structure); _ },
          _,
          _,
          _ );
@@ -196,4 +238,3 @@ let type_structure ~env structure =
       | Tmod_apply _ -> failwith "Tmod_apply impossible"
       | Tmod_constraint _ -> failwith "Tmod_constraint impossible"
       | Tmod_unpack _ -> failwith "Tmod_unpack impossible")
-
